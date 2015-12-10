2172d8421e970da7087920cbf1d3a0b060e6dcd5
Mad Kernel <amd@default.im>
2015-12-10 02:38:20 +0200

    Initial commit

--- lua/entities/trigon-dispenser-cp/cl_init.lua
+++ lua/entities/trigon-dispenser-cp/cl_init.lua
@@ -1,3 +1,21 @@
+/*************************************************************************
+	Полный исходный код этого скрипта можно найти на GitHub
+	Full source code of this script you can find in this GitHub Repo
+
+	Скрипт распостраняется под свободной лицензией Apache 2.0
+	Одним из требований лицензии является уведомление автора при
+	использовании исходного кода продукта, в данном случае - скрипта
+	http://steamcommunity.com/profiles/76561198071463189
+
+	
+	If you want to use this script, you must notify author
+	http://steamcommunity.com/profiles/76561198071463189
+
+	The script is spread under the free Apache 2.0 license
+	One of the requirements of the license is to notify the author
+	if you going to use the source code of this script
+	http://steamcommunity.com/profiles/76561198071463189
+**************************************************************************/
 include("shared.lua")
 
 
